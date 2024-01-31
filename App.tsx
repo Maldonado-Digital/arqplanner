058c0d5786a0e5e8655d04200eb5a83b72e641ab
bernardo.amim <bernardo.amim@mercatus.com>
2024-01-31 00:14:56 -0300

    feat: configuring icon, splash screen and status bar

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+import { StatusBar } from 'expo-status-bar'
+import { useCallback } from 'react'
+import { Text, View } from 'react-native'
+
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded, fontError] = useFonts({
+    MarkPro_Regular: require('./assets/fonts/MarkPro_Regular.otf'),
+    MarkPro_Medium: require('./assets/fonts/MarkPro_Medium.otf'),
+    MarkPro_Bold: require('./assets/fonts/MarkPro_Bold.otf'),
+  })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded, fontError])
+
+  if (!fontsLoaded && !fontError) {
+    return null
+  }
+
+  return (
+    <View
+      onLayout={onLayoutRootView}
+      style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+    >
+      <StatusBar style="dark" />
+      <Text style={{ fontFamily: 'MarkPro_Bold', fontSize: 30 }}>
+        Inter Black
+      </Text>
+      <Text style={{ fontSize: 30 }}>Platform Default</Text>
+    </View>
+  )
+}
