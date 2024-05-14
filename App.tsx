4469bc690315a42656e7b8a8f3544f6766586374
bernardo.amim <bernardo.amim@mercatus.com>
2024-05-14 05:41:26 -0300

    fix: add update caller

--- App.tsx
+++ App.tsx
@@ -6,15 +6,31 @@ import { useFonts } from 'expo-font'
 import { StatusBar } from 'expo-status-bar'
 import { NativeBaseProvider } from 'native-base'
 import 'react-native-gesture-handler'
+// SplashScreen.preventAutoHideAsync()
+import * as Updates from 'expo-updates'
+import { useEffect } from 'react'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { queryClient } from 'src/lib/react-query'
-// SplashScreen.preventAutoHideAsync()
 
 export default function App() {
   const [fontsLoaded, fontError] = useFonts({
     Proxima_Nova_Regular: require('./assets/fonts/Proxima_Nova_Regular.otf'),
     Proxima_Nova_Semibold: require('./assets/fonts/Proxima_Nova_Semibold.otf'),
   })
+
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync()
+
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync()
+        await Updates.reloadAsync()
+      }
+    } catch (error) {
+      // You can also add an alert() to see the error message in case of an error when fetching updates.
+      alert(`Error fetching latest Expo update: ${error}`)
+    }
+  }
 
   // const onLayoutRootView = useCallback(async () => {
   //   if (fontsLoaded || fontError) {
@@ -25,6 +41,11 @@ export default function App() {
   // if (!fontsLoaded && !fontError) {
   //   return null
   // }
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  useEffect(() => {
+    onFetchUpdateAsync()
+  }, [])
 
   return (
     <NativeBaseProvider theme={THEME}>
